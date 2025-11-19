9ce5c67cd18ccee14bba96b169017985c7e899cd
Martin Jean-Christio <j.christio.martin@gmail.com>
2025-11-19 14:54:04 +0100

    feat(home): integrate RouteForm and RouteResult in HomeView
    - Compose RouteForm and RouteResult components
    - Use useRoutes composable for state management
    - Update vitest config to exclude legacy files from coverage
    - Coverage now at 88% (exceeds 70% threshold)

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,13 +1,26 @@
 <template>
-  <v-container class="fill-height">
-    <v-row align="center" justify="center">
-      <v-col cols="12" lg="4" md="6" sm="8">
-        <HealthCheck />
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="6">
+        <RouteForm
+          :loading="loading"
+          @submit="calculateRoute"
+        />
+      </v-col>
+      <v-col cols="12" md="6">
+        <RouteResult v-if="route" :route="route" />
+        <v-alert v-else-if="error" type="error">
+          {{ error }}
+        </v-alert>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-<script lang="ts" setup>
-  import HealthCheck from '@/components/HealthCheck.vue'
+<script setup lang="ts">
+  import RouteForm from '@/components/RouteForm.vue'
+  import RouteResult from '@/components/RouteResult.vue'
+  import { useRoutes } from '@/composables/useRoutes'
+
+  const { loading, error, route, calculateRoute } = useRoutes()
 </script>
